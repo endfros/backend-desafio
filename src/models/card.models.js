3908f08a8334ca9f55a394e743fd28a2e17c0c39
Hector <Hector.bprz@gmail.com>
2022-11-02 21:49:32 -0500

    card's use fixed

--- src/models/card.models.js
+++ src/models/card.models.js
@@ -1,47 +1,117 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const cardSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-        minLenght: 1,
-        maxLenght: 120,
+  title: {
+    type: String,
+    required: true,
+    minLenght: 1,
+    maxLenght: 120,
+  },
+  body: {
+    type: String,
+    required: true,
+  },
+  hashtags: [
+    {
+      type: String,
     },
-    body: {
-        type: String,
-        required: true,
+  ],
+  img: {
+    type: String,
+    trim: true,
+  },
+  reactions: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  readingTime: {
+    type: Number,
+    required: true,
+    default: 1,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users",
+    required: true,
+  },
+  comment: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "comment",
     },
-    hashtags: [{
-        type: String,
-    }],
-    img: {
-        type: String,
-        trim: true
-    },
-    reactions: {
-        type: Number,
-        required: true,
-        default: 0
-    },
-    readingTime: {
-        type: Number,
-        required: true,
-        default: 1
-    },
-    date:{
-        type: Date, 
-        default: Date.now,
-    },
-    user:{
-        type: mongoose.Schema.Types.ObjectId, ref: 'users',
-        required: true
-    },
-    comment:[{
-        type: mongoose.Schema.Types.ObjectId, ref: 'comment' 
-    }]
+  ],
+});
 
-})
+const Card = mongoose.model("cards", cardSchema);
 
-const Card = mongoose.model('cards', cardSchema )
+export { Card };
 
-export {Card}
+/*
+
+user: 
+- Name
+- Lastname
+- BirthDate
+- ZipCode
+- Level ["beg", "inter", "expert"]
+- Friends [ id_users ]
+- FriendsRequests [ id_friendsRequests ]
+- CreatedAt: date
+- UpdatedAt: date
+- RacesCreated: [id_races]
+- RacesRequests: [ id_RaceRequests ]
+
+Race:
+- Title
+- Description
+- User : id_user
+- Location
+- ZipCode (checar con api)
+- Level
+- Km
+- Date
+- hour
+- CreatedAt: date
+- UpdatedAt: date
+- Status ["Finished", "Scheduled"]
+- comments [ id_comment ]
+- Rating
+- Assistants [ id_users ]
+
+
+cron job.
+indexación (modelo)
+bucket ds3
+google maps
+mapbox - https://www.mapbox.com/
+
+Comments:
+- Race: id_race
+- User: id_user
+- Text
+- Image: url del bucket (string)
+- Rate
+- CreatedAt: date
+- UpdatedAt: date
+
+RaceRequests:
+- Race: id_race
+- User: id_user
+- Status: ["Approved", "Pending", "Rejected"]
+
+FriendRequests:
+- userRequester: id_user
+- userResponder: id_user
+- Status: ["Approved", "Pending", "Rejected"]
+
+
+
+
+
+
+*/
--- src/useCase/card.use.js
+++ src/useCase/card.use.js
@@ -1,64 +1,81 @@
-import {Card} from '../models/card.models.js'
-import {StatusHttp} from '../libs/errorCustom.js'
-import jwt from 'jsonwebtoken'
+import { Card } from "../models/card.models.js";
+import { StatusHttp } from "../libs/errorCustom.js";
 
-
-async function getAll(){
-    const data = await Card.find({}).populate('user').populate('comment')
-    if(!data) throw new StatusHttp('There are no posts', 400)
-    return data
+async function getAll() {
+  const data = await Card.find({}).populate("user").populate("comment");
+  if (!data) throw new StatusHttp("There are no posts", 400);
+  return data;
 }
 
-async function getById(id){
-    const data = await Card.findById(id).populate('user').populate('comment')
-    if(!data) throw new StatusHttp('Post not found', 404)
-    return data
+async function getById(id) {
+  const data = await Card.findById(id).populate("user").populate("comment");
+  if (!data) throw new StatusHttp("Post not found", 404);
+  return data;
 }
 
-async function getByUser(id){
-    const data = await Card.find({user: id}).populate('user').populate('comment')
-    if(!data)throw new StatusHttp('User has no post.', 400)
-    return data
+async function getByUser(id) {
+  const data = await Card.find({ user: id })
+    .populate("user")
+    .populate("comment");
+  if (!data) throw new StatusHttp("User has no post.", 400);
+  return data;
 }
 
-async function deleteById(id){
-    const data = await Card.findByIdAndDelete(id).populate('user').populate('comment')
-    if(!data)throw new StatusHttp('Post not found', 404)
-    return data
+async function deleteById(id) {
+  const data = await Card.findByIdAndDelete(id)
+    .populate("user")
+    .populate("comment");
+  if (!data) throw new StatusHttp("Post not found", 404);
+  return data;
 }
 
-async function create (newCard, user){
-    const {title, body, hashtags, img, reactions, readingTime, date} = newCard
-    const data = await Card.create({title, body, hashtags, img, reactions, readingTime, date, user})
-    if(!data)throw new StatusHttp('An error ocurred', 400)
-    return data
+async function create(newCard, user) {
+  const data = await Card.create({ ...newCard, user });
+  if (!data) throw new StatusHttp("An error ocurred", 400);
+  return data;
 }
 
-async function createComment (idCard, idComment){
-    const data = await Card.findByIdAndUpdate(idCard, {$push:{comment: idComment}}, {new : true}).populate('user').populate('comment')
-    if(!data)throw new StatusHttp('An error ocurred', 404)
-    return data
+async function createComment(idCard, idComment) {
+  const data = await Card.findByIdAndUpdate(
+    idCard,
+    { $push: { comment: idComment } },
+    { new: true }
+  )
+    .populate("user")
+    .populate("comment");
+  if (!data) throw new StatusHttp("An error ocurred", 404);
+  return data;
 }
 
-async function deleteComment(idCard, idComment){
-    const data = await Card.findByIdAndUpdate(idCard, {$pull:{comment: idComment}}, {new : true}).populate('user').populate('comment')
-    if(!data)throw new StatusHttp('Post not found', 404)
-    return data
+async function deleteComment(idCard, idComment) {
+  const data = await Card.findByIdAndUpdate(
+    idCard,
+    { $pull: { comment: idComment } },
+    { new: true }
+  )
+    .populate("user")
+    .populate("comment");
+  if (!data) throw new StatusHttp("Post not found", 404);
+  return data;
 }
 
-async function update(idCard, unupdatedCard){
-    const data = await Card.findByIdAndUpdate(idCard, unupdatedCard, {new : true}).populate('user').populate('comment')
-    if(!data)throw new StatusHttp('Post not found', 404)
-    return data
+async function update(idCard, unupdatedCard) {
+  const data = await Card.findByIdAndUpdate(idCard, unupdatedCard, {
+    new: true,
+  })
+    .populate("user")
+    .populate("comment");
+  if (!data) throw new StatusHttp("Post not found", 404);
+  return data;
 }
 
 export {
-    getAll,
-    getById,
-    getByUser,
-    deleteById,
-    update, 
-    create, 
-    createComment,
-    deleteComment
-    }
+  getAll,
+  getById,
+  getByUser,
+  deleteById,
+  update,
+  create,
+  createComment,
+  deleteComment,
+};
